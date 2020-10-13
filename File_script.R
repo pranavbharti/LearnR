357b0fdd3c5c32f168b16ad5334f1d53715431bb
Pranav Bharti <you@example.com>
2020-10-13 22:55:39 +0530

    Third commit

--- File_script.R
+++ File_script.R
@@ -1,1 +1,11 @@
 a=1
+library(devtools)
+install.packages(devtools)
+library(devtools)
+install_github(repo="coefplot/jaredlander")
+install.packages(coefplot)
+library(coefplot)
+library(coefplot, quietly=TRUE)
+detach("package:coefplot")
+2+2
+3+6
